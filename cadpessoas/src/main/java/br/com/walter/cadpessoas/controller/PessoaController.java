7af462a8ae6db4418fe6da18275148c7c864aec2
Walter Souza <walter.junior@alunos.ufersa.edu.br>
2021-04-04 15:47:46 -0300

    fazendo o editar pessoa

--- cadpessoas/src/main/java/br/com/walter/cadpessoas/controller/PessoaController.java
+++ cadpessoas/src/main/java/br/com/walter/cadpessoas/controller/PessoaController.java
@@ -3,7 +3,9 @@ package br.com.walter.cadpessoas.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -40,4 +42,16 @@ public class PessoaController {
 		return "redirect:/listarPessoas";
 	}
 
+	@GetMapping("/editar/{id}")
+	public ModelAndView editarPessoa(@PathVariable Long id) {
+		ModelAndView mav = new ModelAndView("editarPessoa");
+		mav.addObject("pessoa", pessoaRepo.findById(id));
+		return mav;
+	}
+
+	@PostMapping("/editar")
+	public ModelAndView editarPessoa(Pessoa pessoa) {
+		pessoaRepo.save(pessoa);
+		return new ModelAndView("redirect:/listarPessoas");
+	}
 }
