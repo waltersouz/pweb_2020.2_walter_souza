eab346092623c4dd5bd6d424558d41d7da35be62
Walter Souza <walter.junior@alunos.ufersa.edu.br>
2021-04-26 10:05:19 -0300

    iniciando projeto

---
+++ agroPopShopWalter/src/main/java/br/com/walter/agroPopShopWalter/AgroPopShopWalterApplication.java
@@ -0,0 +1,13 @@
+package br.com.walter.agroPopShopWalter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroPopShopWalterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroPopShopWalterApplication.class, args);
+	}
+
+}
---
+++ agroPopShopWalter/src/test/java/br/com/walter/agroPopShopWalter/AgroPopShopWalterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.walter.agroPopShopWalter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroPopShopWalterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
