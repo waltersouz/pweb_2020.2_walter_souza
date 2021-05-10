dc5f7f74a96a7504ab9676aa1a8547f7caa79701
Walter Souza <walter.junior@alunos.ufersa.edu.br>
2021-05-09 22:12:41 -0300

    quase terminando pedidos

--- agroPopShopWalter/src/main/java/br/com/walter/agroPopShopWalter/controller/PedidoController.java
+++ agroPopShopWalter/src/main/java/br/com/walter/agroPopShopWalter/controller/PedidoController.java
@@ -5,11 +5,11 @@ import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.com.walter.agroPopShopWalter.model.Cliente;
 import br.com.walter.agroPopShopWalter.model.Pedido;
 import br.com.walter.agroPopShopWalter.repository.ClienteRepository;
 import br.com.walter.agroPopShopWalter.repository.PedidoRepository;
@@ -35,7 +35,7 @@ public class PedidoController {
 		mv.addObject("clientes", clienteRepo.findAllByOrderByNomeCompletoAsc());
 		return mv;
 	}
-	
+
 	@PostMapping(value = "/adicionaPedido")
 	public String adicionaPedido(Pedido pedido) {
 		pedido.setValorUnitario(produtoRepo.getOne(pedido.getProduto().getCodigo()).getPreco());
@@ -43,11 +43,33 @@ public class PedidoController {
 		pedidoRepo.save(pedido);
 		return "redirect:/pedido/listarPedidos";
 	}
-	
+
 	@GetMapping(value = "/listarPedidos")
 	public ModelAndView listar() {
 		ModelAndView mv = new ModelAndView("listarPedidos");
 		mv.addObject("pedidos", pedidoRepo.findAll());
 		return mv;
+	}
+
+	@GetMapping(value = "/editar/{id}")
+	public ModelAndView editarTela(@PathVariable Long id) {
+		ModelAndView mv = new ModelAndView("editarPedido");
+		mv.addObject("produtos", produtoRepo.findAllByOrderByNomeAsc());
+		mv.addObject("clientes", clienteRepo.findAllByOrderByNomeCompletoAsc());
+		mv.addObject("pedido", pedidoRepo.getOne(id));
+		return mv;
+	}
+	
+	@PostMapping(value = "/editar")
+	public String editarPedido(Pedido pedido) {
+		pedido.setValorUnitario(produtoRepo.getOne(pedido.getProduto().getCodigo()).getPreco());
+		pedidoRepo.save(pedido);
+		return "redirect:/pedido/listarPedidos";
+	}
+	
+	@GetMapping(value = "/remover/{id}")
+	public String remover(@PathVariable Long id) {
+		pedidoRepo.delete(pedidoRepo.getOne(id));
+		return "redirect:/pedido/listarPedidos";
 	}
 }
